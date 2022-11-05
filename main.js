30e6b5c024e63c2f378c8c63eff01416b34fd1fa
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2022-11-05 13:46:40 -0300

    refactor(css): changed PX to REM and added more games

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function createGame(player1, hour, player2) {
 
 let delay = -0.3;
 function createCard(date, day, games) {
-  delay = delay + 0.3
+  delay = delay + 0.3;
   return `
     <div class="card" style='animation-delay: ${delay}s'>
       <h2>${date}<span>${day}</span></h2>
@@ -22,11 +22,27 @@ function createCard(date, day, games) {
 }
 
 document.querySelector('.cards').innerHTML =
-  createCard('24/11', 'quinta', createGame('brazil', '16:00', 'serbia')) +
+  createCard('22/11', 'terça', createGame('france', '16:00', 'australia')) +
+  createCard(
+    '24/11',
+    'quinta',
+    createGame('uruguay', '10:00', 'southkorea') +
+      createGame('portugal', '13:00', 'ghana') +
+      createGame('brazil', '16:00', 'serbia')
+  ) +
+  createCard('26/11', 'sábado', createGame('france', '13:00', 'denmark')) +
   createCard(
     '28/11',
     'segunda',
-    createGame('brazil', '13:00', 'switzerland') +
+    createGame('southkorea', '10:00', 'ghana') +
+      createGame('brazil', '13:00', 'switzerland') +
       createGame('portugal', '16:00', 'uruguay')
   ) +
-  createCard('02/12', 'sexta', createGame('cameroon', '16:00', 'brazil'));
+  createCard('30/11', 'quarta', createGame('tunisia', '12:00', 'france')) +
+  createCard(
+    '02/12',
+    'sexta',
+    createGame('southkorea', '12:00', 'portugal') +
+      createGame('ghana', '12:00', 'uruguay') +
+      createGame('cameroon', '16:00', 'brazil')
+  );
