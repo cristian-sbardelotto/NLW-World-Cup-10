ce70a7bf39019cb44e0ea350644a26d43747938c
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2022-11-06 11:32:17 -0300

    refactor(css): HEX color to HSL color and responsiveness

--- src/main.js
+++ src/main.js
@@ -24,12 +24,12 @@ let delay = -0.3;
 function createCard(date, day, games) {
   delay = delay + 0.3;
   return `
-    <div class="card" style='animation-delay: ${delay}s'>
+    <blockquote class="card" style='animation-delay: ${delay}s'>
       <h2>${date}<span>${day}</span></h2>
       <ul>
         ${games}
       </ul>
-    </div>
+    </blockquote>
   `;
 }
 
